3c407c2ae4a39eaee61cc0f35a94ba90779616e2
ihealth861 <ihealth861@gmail.com>
2023-05-22 20:03:27 +0100

    billet commit

--- lib/widgets/eventUser/liste_billets.dart
+++ lib/widgets/eventUser/liste_billets.dart
@@ -4,6 +4,115 @@ import 'package:flutter_feather_icons/flutter_feather_icons.dart';
 
 class ListeBillets extends StatelessWidget {
   static const routeName = '/Listebillets';
+   final _formKey = GlobalKey<FormState>();
+  TextEditingController nomController= TextEditingController();
+  TextEditingController quantite= TextEditingController();
+   void _showBottomSheet(BuildContext context) {
+    showModalBottomSheet(
+      isScrollControlled: true,
+      backgroundColor: Colors.transparent,
+      context: context,
+      builder: (context) => buildsheet(),
+    );
+  }
+
+  Widget makeDismissible({required Widget child}) => Builder(
+    builder: (context) {
+      return GestureDetector(
+        behavior: HitTestBehavior.opaque,
+        onTap: () => Navigator.of(context).pop(),
+        child: GestureDetector(onTap: () {}, child: child,),
+      );
+    }
+  );
+
+  Widget buildsheet () => makeDismissible(
+    child: DraggableScrollableSheet(
+      initialChildSize: 0.9,
+      maxChildSize: 0.9,
+        builder: (_, controller) => Container(
+          padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+          decoration: const BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.vertical(top: Radius.circular(15))
+          ),
+          child: ListView(
+            controller: controller,
+            children: [
+              Center(child: 
+              Container(
+                padding: const EdgeInsets.all(10.0),
+                child: Text(
+                  'Ajouter un billet',style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black.withOpacity(0.8)
+                  ),
+                ),
+              ),),
+              
+              const SizedBox(height: 8,),
+
+             Form(
+            key: _formKey,
+            child: Column(
+            children: [
+     
+      const SizedBox(height: 15,),
+          
+            TextFormField(
+            controller: nomController,
+             keyboardType: TextInputType.name,
+             decoration: const InputDecoration(
+              hintText: "Entrer votre nom",
+              hintStyle: TextStyle(
+                color: Colors.grey,
+                fontSize: 18
+                ),
+              label: Text('nom du billet'), 
+               
+             ),
+             validator: (value){
+                        if(value ==null || value.isEmpty){
+                          return 'Veuillez entrez le nom de votre évènement';
+                        }
+                        return null;
+                      },
+             
+            ),
+
+            const SizedBox(height: 15,),
+
+            TextFormField(
+            controller: quantite,
+             keyboardType: TextInputType.number,
+             decoration: const InputDecoration(
+              hintText: "0",
+              hintStyle: TextStyle(
+                color: Colors.grey,
+                fontSize: 18
+                ),
+              label: Text('Quantité'), 
+               
+             ),
+             validator: (value){
+                        if(value ==null || value.isEmpty){
+                          return 'Veuillez entrez le nom de votre évènement';
+                        }
+                        return null;
+                      },
+             
+            ),
+            ],
+            
+          )),
+
+            ],
+          ),
+        )
+    ),
+  );
+
 
   @override
   Widget build(BuildContext context) {
@@ -11,7 +120,7 @@ class ListeBillets extends StatelessWidget {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
     child: const Icon(FeatherIcons.plus),
-    onPressed: () {Navigator.of(context).pushNamed('/AddBillet', arguments: routeArgs);}),
+    onPressed: () {_showBottomSheet(context);}),
       appBar: AppBar(
         title: const Text("Gerer les billets"),
         elevation: 0,
