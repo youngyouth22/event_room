2d933a1eaa125eb4c0cf45e2244adfd39566401b
ihealth861 <ihealth861@gmail.com>
2023-05-22 13:01:08 +0100

    first commit

--- lib/widgets/home_widget/categorie_chip.dart
+++ lib/widgets/home_widget/categorie_chip.dart
@@ -24,7 +24,12 @@ class _CategorieChipsState extends State<CategorieChips> {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 4.0),
       child: ChoiceChip(
-        shape: const RoundedRectangleBorder(borderRadius: BorderRadius.only()),
+        shape: const RoundedRectangleBorder(borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(6),
+          topRight: Radius.circular(6),
+          bottomLeft: Radius.circular(6),
+          bottomRight: Radius.circular(6),
+        )),
         labelStyle: TextStyle(color: _selectedIndex == index ? Colors.white :Colors.black45, fontSize: 14),
         labelPadding: const EdgeInsets.symmetric(vertical: 2, horizontal: 10),
         selectedColor: ColorApp.secondaryColor,
